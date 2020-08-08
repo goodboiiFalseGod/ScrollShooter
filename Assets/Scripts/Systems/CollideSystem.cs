6b781821a81a9e78c0c99f26dd47aa45cd7a53e1
goodboiiFalseGod <59483418+goodboiiFalseGod@users.noreply.github.com>
2020-08-08 16:13:00 +0300

    One more collide system fix

--- Assets/Scripts/Systems/CollideSystem.cs
+++ Assets/Scripts/Systems/CollideSystem.cs
@@ -16,14 +16,15 @@ namespace Client {
             foreach(var index in _asteroidFilter)
             {
                 ref TransformRef asteroidTransformRefComponent = ref _asteroidFilter.Get2(index);
-                ref ColliderRadius colliderRadiusComponent = ref _asteroidFilter.Get3(index);
+                ref ColliderRadius asteroidColliderRadiusComponent = ref _asteroidFilter.Get3(index);
 
                 foreach(var index1 in _bulletFilter)
                 {
                     ref TransformRef bulletTransfromRefComponent = ref _bulletFilter.Get2(index1);
+                    ref ColliderRadius bulletColliderRadiusComponent = ref _bulletFilter.Get3(index1);
                     float dis = Vector3.Distance(asteroidTransformRefComponent.value.position, bulletTransfromRefComponent.value.position);
 
-                    if(dis < colliderRadiusComponent.value)
+                    if(dis < asteroidColliderRadiusComponent.value + bulletColliderRadiusComponent.value)
                     {
                         _asteroidFilter.GetEntity(index).Get<Collided>();
                         _bulletFilter.GetEntity(index1).Get<Collided>();
@@ -33,10 +34,10 @@ namespace Client {
                 foreach(var index1 in _playerFilter)
                 {
                     ref TransformRef playerTransformRefComponent = ref _playerFilter.Get2(index1);
-                    ref ColliderRadius colliderRadiusComponent1 = ref _asteroidFilter.Get3(index);
+                    ref ColliderRadius playerColliderRadiusComponent = ref _asteroidFilter.Get3(index);
                     float dis = Vector3.Distance(asteroidTransformRefComponent.value.position, playerTransformRefComponent.value.position);
 
-                    if(dis < colliderRadiusComponent1.value)
+                    if (dis <= playerColliderRadiusComponent.value + asteroidColliderRadiusComponent.value)
                     {
                         _asteroidFilter.GetEntity(index).Get<Collided>();
                         _playerFilter.GetEntity(index1).Get<Collided>();
